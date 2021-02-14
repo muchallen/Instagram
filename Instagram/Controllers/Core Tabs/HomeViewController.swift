52e3456c2aeb6fc78e16be4fdd413cfe678ba014
muchallen <muchallen@gmail.com>
2021-02-15 00:00:08 +0200

    first commit

---
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  Instagram
+//
+//  Created by mac on 14/2/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
